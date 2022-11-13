1709fa32150831a477456c07cea4e852999f82a2
Dawit Tadesse <davizsremurd927@gmail.com>
2022-11-13 17:20:38 +0300

    fix: wrap the app with stream-builder to listen for changes and switch between pages automatically

--- lib/todo_app.dart
+++ lib/todo_app.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app_flutter/components/constants.dart';
-import 'package:todo_app_flutter/onboarding_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_app_flutter/screens/home.dart';
+import 'package:todo_app_flutter/screens/registration.dart';
 
 class TodoApp extends StatelessWidget {
   const TodoApp({super.key});
-
+  static const String pageId = '/todoApp';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.from(
-        colorScheme: const ColorScheme.light().copyWith(
-          primary: kPrimaryColor,
-        ),
-      ),
-      home: const OnboardingScreen(),
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          return const HomePage();
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Something went Wrong!'));
+        }
+        return const RegistrationPage();
+      },
     );
   }
 }
