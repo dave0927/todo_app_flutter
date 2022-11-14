e0a124b1e1d4dd13977f35e68fd23fb599153fc2
Dawit Tadesse <davizsremurd927@gmail.com>
2022-11-14 23:53:25 +0300

    refactor: all 3 pages that contain tasks and addTask button FAB

---
+++ lib/widgets/task/task_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_flutter/components/constants.dart';
+import 'package:todo_app_flutter/widgets/add_item.dart';
+
+class TaskList extends StatelessWidget {
+  const TaskList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        bottomSheet: TabBar(tabs: kBottomSheetTextItems),
+        body: TabBarView(children: pages),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: const AddItem(),
+      ),
+    );
+  }
+}
