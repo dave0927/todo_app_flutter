e7ad59ed4578050076840bcfe6e454e4c6ee30ec
Dawit Tadesse <davizsremurd927@gmail.com>
2022-11-15 13:31:58 +0300

    fix: remove unnecessary parent widget

--- lib/widgets/task/task_list_builder.dart
+++ lib/widgets/task/task_list_builder.dart
@@ -7,25 +7,22 @@ class TaskListBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      child: Expanded(
-        child: ListView.builder(
-          itemCount: 4, //TODO: make it dynamic
-          scrollDirection: Axis.vertical,
-          itemBuilder: (context, index) {
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              elevation: 4.0,
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-              child: CreateTaskTile(
-                taskText: 'Task ${index + 1}',
-              ),
-            );
-          },
-        ),
+      padding: const EdgeInsets.all(20.0),
+      child: ListView.builder(
+        itemCount: 4, //TODO: make it dynamic
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 4.0,
+            margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+            child: CreateTaskTile(
+              taskText: 'Task ${index + 1}',
+            ),
+          );
+        },
       ),
     );
   }
