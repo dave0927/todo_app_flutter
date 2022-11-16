e7aa96e3cd150f84de62a97e3d71f9880a6bfc1c
Dawit Tadesse <davizsremurd927@gmail.com>
2022-11-16 21:10:52 +0300

    fix! : add a TASK object & isDone boolean in order to be able to check off the task and add text deccoration

--- lib/widgets/task/task_tile.dart
+++ lib/widgets/task/task_tile.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app_flutter/components/constants.dart';
-import 'package:todo_app_flutter/widgets/task/edit_task_button.dart';
+import 'package:todo_app_flutter/models/task_model.dart';
 import 'package:todo_app_flutter/widgets/task/leading_icon.dart';
 import 'package:todo_app_flutter/widgets/task/task_subtitle.dart';
+import 'package:todo_app_flutter/widgets/task/edit_task_button.dart';
 
 class CreateTaskTile extends StatelessWidget {
   const CreateTaskTile({
     Key? key,
     required this.taskText,
+    required this.isDone,
+    required this.task,
   }) : super(key: key);
-
+  final bool isDone;
+  final Task task;
   final String taskText;
 
   @override
@@ -19,8 +23,14 @@ class CreateTaskTile extends StatelessWidget {
         horizontal: 10.0,
         vertical: 10.0,
       ),
-      leading: const TaskLeadingIcon(isCompleted: false),
-      title: Text(taskText, style: kTaskTextStyle),
+      leading: TaskLeadingIcon(
+        isCompleted: isDone,
+        task: task,
+      ),
+      title: Text(
+        taskText,
+        style: isDone ? kTaskDoneTextStyle : kTaskTextStyle,
+      ),
       subtitle: const TaskTileSubtitle(),
       trailing: EditTaskButton(
         onEditTaskCalled: () {
