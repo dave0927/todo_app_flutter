25ece5a1698e0ac4184d503a05b379ad289e4fc3
Dawit Tadesse <davizsremurd927@gmail.com>
2022-11-18 00:50:58 +0300

    feat: use consumer to build TaskListBuilder

--- lib/widgets/task/task_list_builder.dart
+++ lib/widgets/task/task_list_builder.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app_flutter/providers/task_data_provider.dart';
 import 'package:todo_app_flutter/widgets/task/task_tile.dart';
 
 class TaskListBuilder extends StatelessWidget {
@@ -6,24 +8,40 @@ class TaskListBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20.0),
-      child: ListView.builder(
-        itemCount: 4, //TODO: make it dynamic
-        scrollDirection: Axis.vertical,
-        itemBuilder: (context, index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 4.0,
-            margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-            child: CreateTaskTile(
-              taskText: 'Task ${index + 1}',
-            ),
-          );
-        },
-      ),
+    return Consumer<TaskData>(
+      builder: (BuildContext context, taskData, Widget? child) {
+        return taskData.taskCouter != 0
+            ? Container(
+                padding: const EdgeInsets.all(20.0),
+                child: ListView.builder(
+                  //TODO: display all task that are completed & not completed
+                  itemCount: taskData.taskCouter,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      elevation: taskData.tasks[index].isDone ? 2.0 : 4.0,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 6.0),
+                      color: taskData.tasks[index].isDone
+                          ? Colors.grey.shade200
+                          : Colors.white,
+                      child: CreateTaskTile(
+                        task: taskData.tasks[index],
+                      ),
+                    );
+                  },
+                ),
+              )
+            : const Center(
+                child: Text(
+                  'No task available',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              );
+      },
     );
   }
 }
